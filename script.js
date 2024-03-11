b3f0f0ed5fe64fb0ec761d0f873c015cfbdec4d7
prof Edney <111137546+profedney@users.noreply.github.com>
2024-03-11 11:44:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function test(){
 }
 
 function calcula(){
+    //criar variavel
     let num1;
+    //recebe texto do html
     num1=document.getElementById("numero1").value;
+    //transforma texto em numero real
+    let num1real=parseFloat(num1);
+
     let num2;
     num2=document.getElementById("numero2").value;
+    //transformar texto para numero real
+    let num2real=parseFloat(num2);
+
     let result;
-    result=num1+num2;
+    result=num1real+num2real;//atualizar nomes variaveis
 
     document.getElementById("resultado").innerHTML=result;
 }
